0e7792ff1251806d3eccd0f0b8191064e2ec8fd1
Ankit <54770456+Ankit-lather@users.noreply.github.com>
2022-06-03 21:58:40 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copywrite &copy; {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
